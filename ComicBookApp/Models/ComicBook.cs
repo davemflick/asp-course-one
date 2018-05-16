85f1efea750e81ca4885b14895c5aa7cdaf052bd
davemflick <davemflick@yahoo.com>
2018-05-16 15:35:03 -0400

    Updated comic book model

--- ComicBookApp/Models/ComicBook.cs
+++ ComicBookApp/Models/ComicBook.cs
@@ -16,5 +16,21 @@ namespace ComicBookApp.Models
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public ComicBook Favorite { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
     }
 }
