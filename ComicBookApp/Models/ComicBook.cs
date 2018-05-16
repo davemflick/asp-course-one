649685b06bac07d496a64163237669ddae576819
davemflick <davemflick@yahoo.com>
2018-05-16 15:28:57 -0400

    Added comic book and artist models

---
+++ ComicBookApp/Models/ComicBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace ComicBookApp.Models
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ComicBook
+    {
+        public int ID { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artist[] Artists { get; set; }
+        public ComicBook Favorite { get; set; }
+    }
+}
