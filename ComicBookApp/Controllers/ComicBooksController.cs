1bbbdb36f3a5c9aee1c9551a382f502025d2aff8
davemflick <davemflick@yahoo.com>
2018-05-17 14:44:56 -0400

    updated ids on comic books

--- ComicBookApp/Controllers/ComicBooksController.cs
+++ ComicBookApp/Controllers/ComicBooksController.cs
@@ -3,35 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using System.Web;
+using ComicBookApp.Data;
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ComicBookApp.Controllers
 {
     public class ComicBooksController : Controller
     {
+        private ComicBookRepository _comicBookRepository = null;
+
+        public ComicBooksController()
+        {
+            _comicBookRepository = new ComicBookRepository();
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public ActionResult Detail()
-        {
-            var comicBook = new Models.ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Models.Artist[]
-                {
-                    new Models.Artist() {Name = "Dan Slott", Role = "Script"},
-                    new Models.Artist() {Name = "Humberto Ramos", Role = "Pencils"},
-                    new Models.Artist() {Name = "Victor Olazaba", Role = "Inks"},
-                    new Models.Artist() {Name = "Edgar Delgado", Role = "Colors"},
-                    new Models.Artist() {Name = "Chris Eliopoulos", Role = "Letters"}
-                }
-            };
 
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                id = 1;
+            }
+           
+            var comicBook = _comicBookRepository.GetComicBook((int)id);
+            if(comicBook == null)
+            {
+                id = 1;
+                comicBook = _comicBookRepository.GetComicBook((int)id);
+            }
             return View(comicBook);
         }
 
